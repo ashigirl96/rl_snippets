362863cb55f4a603a8680177bae5a86d8a276904
ashigirl96 <s1511392@u.tsukuba.ac.jp>
2018-04-05 00:31:46 +0900

    Impl socre function of REINFORCE.

--- experimental/score_function.py
+++ experimental/score_function.py
@@ -4,28 +4,43 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import gym
+import numpy as np
 import tensorflow as tf
 from tensorflow.contrib.eager.python import tfe
 
 
-def compute_z(mu):
-  eps = tf.distributions.Normal(0., 1.)
-  z = mu + eps.stddev() * eps.sample()
-  return z
+class Policy(object):
+  
+  def __init__(self):
+    """Define the layers used, during initialization."""
+    self.dense = tf.layers.Dense(10, activation=tf.nn.relu)
+    self.output_layer = tf.layers.Dense(2, activation=None)
+  
+  def predict(self, observ):
+    hidden = self.dense(observ)
+    logits = self.output_layer(hidden)
+    return tf.nn.softmax(logits)
 
 
-def grad(q: tf.distributions.Distribution, mu):
-  with tfe.GradientTape() as tape:
-    z = compute_z(mu)
-    prob = q.log_prob(tf.stop_gradient(z))
-  return tape.gradient(prob, [mu])
+def loss_fn(model: Policy, observ):
+  action_prob = model.predict(observ)
+  log_prob = -tf.log(action_prob)
+  return log_prob
 
 
 def main(_):
-  mu = tfe.Variable(5.)
-  q = tf.distributions.Normal(mu, 1.)
-  gradients = grad(q, mu)
-  print(gradients)
+  policy = Policy()
+  optimizer = tf.train.GradientDescentOptimizer(5e-1)
+  policy_gradient = tfe.implicit_gradients(loss_fn)
+  
+  env = gym.make('CartPole-v0')
+  observs = [env.reset()]
+  for i in range(5):
+    observs.append(env.step(env.action_space.sample())[0])
+  observs = tf.constant(tf.stack(observs))
+  optimizer.apply_gradients(policy_gradient(policy, observs))
+  print(loss_fn(policy, observs))
 
 
 if __name__ == '__main__':
